0107cc59b506486c0136c9a5ae0eedd468f79ac4
Florian Sandel <fsandel@student.42heilbronn.de>
2023-05-30 15:10:29 +0200

    updated signForm function, so it fits with new exceptions

--- CPP05/ex01/Bureaucrat.cpp
+++ CPP05/ex01/Bureaucrat.cpp
@@ -79,12 +79,19 @@ void Bureaucrat::signForm(Form &form) const {
   std::cout << this->_name << " with grade " << this->_grade
             << " trying to sign " << form.getName() << " with GradeToSign "
             << form.getReqGradeSign() << std::endl;
-  if (form.getSigned() == true)
-    std::cout << this->_name << " couldn't sign " << form.getName()
-              << " because it's already signed" << std::endl;
-  else if (this->_grade > form.getReqGradeSign())
-    std::cout << this->_name << " couldn't sign " << form.getName()
-              << " because their grade is too low" << std::endl;
-  else
+  try {
     form.beSigned(*this);
+  } catch (Form::AlreadySignedException &) {
+    std::cout << this->_name << " couldn't sign " << form.getName()
+              << " because "
+              << "it's already signed" << std::endl;
+  } catch (Form::GradeTooLowException &) {
+    std::cout << this->_name << " couldn't sign " << form.getName()
+              << " because "
+              << "their grade is too low" << std::endl;
+  } catch (std::exception &) {
+    std::cout << this->_name << " couldn't sign " << form.getName()
+              << " because "
+              << "this one should't show" << std::endl;
+  }
 }
