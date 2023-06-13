c86040a6b1a1c2c6ef83f8ab6a6da9d5d5d4e986
Florian Sandel <fsandel@student.42heilbronn.de>
2023-06-13 09:17:31 +0200

    initial commit

---
+++ CPP08/ex01/Span.hpp
@@ -0,0 +1,14 @@
+#ifndef SPAN_HPP_
+#define SPAN_HPP_
+
+class Span {
+ public:
+  Span();
+  ~Span();
+  Span(const Span & obj);
+  Span& operator=(const Span& obj);
+
+ private:
+};
+
+#endif  // SPAN_HPP_
