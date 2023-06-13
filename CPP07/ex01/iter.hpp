9ee7d166a274869b4f7d0e2b783f056c7f464136
Florian Sandel <fsandel@student.42heilbronn.de>
2023-06-13 14:10:54 +0200

    made it work with given main

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,5 +1,5 @@
-template <typename Type>
-void iter(Type* address, std::size_t size, void (*func)(Type&)) {
+template <typename Type, typename Ret>
+void iter(Type* address, std::size_t size, Ret func) {
   for (std::size_t i = 0; i < size; i++) {
     func(address[i]);
   }
