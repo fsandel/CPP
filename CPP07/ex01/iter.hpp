6b0d92173c7815439633896b86242593325beb01
Florian Sandel <fsandel@student.42heilbronn.de>
2023-06-14 13:06:54 +0200

    added header

--- CPP07/ex01/iter.hpp
+++ CPP07/ex01/iter.hpp
@@ -1,6 +1,13 @@
+#ifndef ITER_HPP_
+#define ITER_HPP_
+
+#include <cstddef>
+
 template <typename Type, typename Ret>
 void iter(Type* address, std::size_t size, Ret func) {
   for (std::size_t i = 0; i < size; i++) {
     func(address[i]);
   }
 }
+
+#endif  // ITER_HPP_
