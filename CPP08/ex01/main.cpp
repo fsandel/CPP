35f99eb85ef9976a3e441a65f6da79d8bf08a0c3
Florian Sandel <fsandel@student.42heilbronn.de>
2023-06-14 11:02:06 +0200

    fixed style

--- CPP08/ex01/main.cpp
+++ CPP08/ex01/main.cpp
@@ -3,15 +3,15 @@
 #include "Span.hpp"
 
 int main(void) {
-  int arr[] = { 1, 2, 3, 4, 5 };
+  int arr[] = {1, 2, 3, 4, 5};
   std::vector<int> ar(arr, arr + 5);
   Span test(10);
   test.addIter(ar.begin(), ar.end());
-  //test.addRange(3, 5);
-  //test.addRange(2);
-  //test.addNumber(-5);
+  // test.addRange(3, 5);
+  // test.addRange(2);
+  // test.addNumber(-5);
   test.randomFill(100);
-  //test.addRange(14);
+  // test.addRange(14);
 
   std::cout << test << std::endl;
   std::cout << "longestSpan() " << test.longestSpan() << std::endl;
