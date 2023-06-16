c90a9ae6d448fc1c203234334b9f5b86cdafb6fa
Florian Sandel <fsandel@student.42heilbronn.de>
2023-06-16 10:39:12 +0200

    added first implementation of class

--- CPP09/ex01/RPN.cpp
+++ CPP09/ex01/RPN.cpp
@@ -4,9 +4,16 @@ RPN::RPN() {}
 
 RPN::~RPN() {}
 
-RPN::RPN(const RPN & obj) { *this = obj; }
+RPN::RPN(const RPN& obj) { *this = obj; }
 
 RPN& RPN::operator=(const RPN& obj) {
-  (void)obj;
+  this->nbrs_ = obj.nbrs_;
   return *this;
+}
+
+RPN::RPN(std::string str_represenation) {
+  if (is_valid_rpn_string(str_represenation))
+    std::cout << "valid" << std::endl;
+  else
+    std::cout << "you f'd up" << std::endl;
 }
--- CPP09/ex01/RPN.hpp
+++ CPP09/ex01/RPN.hpp
@@ -1,14 +1,30 @@
 #ifndef RPN_HPP_
 #define RPN_HPP_
 
+#include <iostream>
+#include <iterator>
+#include <stack>
+#include <stdexcept>
+#include <string>
+
 class RPN {
  public:
-  RPN();
   ~RPN();
-  RPN(const RPN & obj);
+  RPN(const RPN& obj);
   RPN& operator=(const RPN& obj);
+  RPN(std::string str_represenation);
 
  private:
+  std::stack<int> nbrs_;
+
+  RPN();
+  static bool is_valid_rpn_char(std::string::iterator iter);
+  static bool is_valid_rpn_string(std::string str_represenation);
+
+  void add();
+  void multiply();
+  void divide();
+  void subtract();
 };
 
 #endif  // RPN_HPP_
