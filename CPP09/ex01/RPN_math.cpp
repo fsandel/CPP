ac7bb737c30f9989380eafdcfd111b3dc61749fb
Florian Sandel <fsandel@student.42heilbronn.de>
2023-06-28 13:07:26 +0200

    added rule for internal overflow

--- CPP09/ex01/RPN_math.cpp
+++ CPP09/ex01/RPN_math.cpp
@@ -3,9 +3,11 @@
 void RPN::divide() {
   if (this->nbrs_.size() < 2)
     throw std::length_error("Not enough arguments during dividing");
-  int tmp = this->nbrs_.top();
+  long tmp = this->nbrs_.top();
   this->nbrs_.pop();
   tmp = this->nbrs_.top() / tmp;
+  if (tmp > INT_MAX) throw std::length_error("Internal overflow");
+  if (tmp < INT_MIN) throw std::length_error("Internal underflow");
   this->nbrs_.pop();
   this->nbrs_.push(tmp);
 }
@@ -13,9 +15,11 @@ void RPN::divide() {
 void RPN::subtract() {
   if (this->nbrs_.size() < 2)
     throw std::length_error("Not enough arguments during subtracting");
-  int tmp = this->nbrs_.top();
+  long tmp = this->nbrs_.top();
   this->nbrs_.pop();
   tmp = this->nbrs_.top() - tmp;
+  if (tmp > INT_MAX) throw std::length_error("Internal overflow");
+  if (tmp < INT_MIN) throw std::length_error("Internal underflow");
   this->nbrs_.pop();
   this->nbrs_.push(tmp);
 }
@@ -23,9 +27,11 @@ void RPN::subtract() {
 void RPN::add() {
   if (this->nbrs_.size() < 2)
     throw std::length_error("Not enough arguments during adding");
-  int tmp = this->nbrs_.top();
+  long tmp = this->nbrs_.top();
   this->nbrs_.pop();
   tmp = this->nbrs_.top() + tmp;
+  if (tmp > INT_MAX) throw std::length_error("Internal overflow");
+  if (tmp < INT_MIN) throw std::length_error("Internal underflow");
   this->nbrs_.pop();
   this->nbrs_.push(tmp);
 }
@@ -33,9 +39,11 @@ void RPN::add() {
 void RPN::multiply() {
   if (this->nbrs_.size() < 2)
     throw std::length_error("Not enough arguments during multiplying");
-  int tmp = this->nbrs_.top();
+  long tmp = this->nbrs_.top();
   this->nbrs_.pop();
   tmp = this->nbrs_.top() * tmp;
+  if (tmp > INT_MAX) throw std::length_error("Internal overflow");
+  if (tmp < INT_MIN) throw std::length_error("Internal underflow");
   this->nbrs_.pop();
   this->nbrs_.push(tmp);
 }
