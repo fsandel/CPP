65d912023c859366ab858e4b8e54361f44438720
Florian Sandel <fsandel@student.42heilbronn.de>
2023-10-26 11:22:28 +0200

    added main from subject

--- CPP04/ex03/main.cpp
+++ CPP04/ex03/main.cpp
@@ -22,13 +22,18 @@
 int main() {
   IMateriaSource* src = new MateriaSource();
   src->learnMateria(new Ice());
-  Character me("me");
+  src->learnMateria(new Cure());
+  ICharacter* me = new Character("me");
   AMateria* tmp;
   tmp = src->createMateria("ice");
-  me.equip(tmp);
-  Character bob("bob");
-  bob = me;
-  bob.use(0, bob);
+  me->equip(tmp);
+  tmp = src->createMateria("cure");
+  me->equip(tmp);
+  ICharacter* bob = new Character("bob");
+  me->use(0, *bob);
+  me->use(1, *bob);
+  delete bob;
+  delete me;
   delete src;
   return 0;
 }
